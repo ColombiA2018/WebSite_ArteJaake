d67d22ba786e5310abbb117df1102a31a8ada4ce
ColombiA2018 <aramirezcarranza1@gmail.com>
2022-07-13 19:22:10 -0500

    inicio del proyecto, pagina web azure

---
+++ wwebsite_artejaake/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wwebsite_artejaake.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
